2f5b23665d97545f0a052069ba1b906a08891a64
majadlymhmd <mohammad.majadly@isst.fraunhofer.de>
2025-02-12 17:28:26 +0100

    chore: update contributor name in header

--- extensions/common/iam/oauth2/oauth2-core/src/main/java/org/eclipse/edc/iam/oauth2/Oauth2ServiceExtension.java
+++ extensions/common/iam/oauth2/oauth2-core/src/main/java/org/eclipse/edc/iam/oauth2/Oauth2ServiceExtension.java
@@ -9,7 +9,7 @@
  *
  *  Contributors:
  *       Microsoft Corporation - initial API and implementation
- *       Fraunhofer Institute for Software and Systems Engineering - Improvements
+ *       Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V. - Improvements
  *       sovity GmbH - added issuedAt leeway
  *
  */
